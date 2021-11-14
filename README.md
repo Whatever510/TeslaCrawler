2aa793a6de0b2405d31ff48f567958e90878f358
Adrian <adrian@ries-ab.de>
2021-11-15 00:53:47 +0100

    Added sitemap pull

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # TeslaCrawler
 Crawl the Tesla configurator website to spot changes in the source code.
 
+##Pull the configurator code
+
 Pull the source code for the 4 Tesla models from the configurator.  
 
 Starting with the second pull the algorithm will create a diff-file to the source code of the previous day.
-## Setup
+### Setup
 Install the necessary requirements
 ```
 pip install jsbeautifier
@@ -13,11 +15,23 @@ pip install beautifulsoup4
 pip install urllib3
 ```
 
-## Using the code
+### Using the code
 1. Clone the Repo
 2. Run:
 
 ```
 python websitecrawl.py
 ```
-Note: You need the pulls of at least two different days to generate a diff file
+Note: You need the pulls of at least two different days to generate a diff file.
+
+## Pull the website of the sitemap
+
+Tesla has a xml sitemap. The sitemap can be found at [Tesla Sitemap Overview](https://www.tesla.com/sitemap.xml).
+To pull the websites listed in the sitemap run:
+
+```
+python xmlpull.py
+```
+
+Note: Certain Websites are skipped, like supercharger location and Websites from certain regions.
+    Pulling takes approximately 60 minutes. The websites are saved in the "xmlPages" Folder. 
--- xmlpull.py
+++ xmlpull.py
@@ -3,17 +3,57 @@ import urllib.request
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import requests
+import os
+from tqdm import tqdm
 
-def extract_urls(soup, url_list):
+"""
+Setup the workspace by creating the necessary folder
+"""
+def setup():
+    content = os.listdir()
+
+    if not ("xmlPages" in content):
+        os.mkdir("xmlPages/")
+    print("[INFO] Setup done - Directory successfully created")
+
+"""
+Extract the URLs from the soup element.
+@param soup: The soup element.
+@param url_list: The list which will contain the extracted -> used as return value.
+@param update_list: The list of last updates - extracted but not used -> used as return value.
+"""
+def extract_urls(soup, url_list, update_list):
     scripts = list(soup.find_all("loc"))
-
-
+    updates = list(soup.find_all("lastmod"))
+    counter = 0
     for i in range(len(scripts)):
         url_string = str(scripts[i]).replace("<loc>","").replace("</loc>","")
 
         url_list.append(url_string)
 
+    for i in range(len(updates)):
+        update_string = str(updates[i]).replace("<lastmod>","").replace("</lastmod>","")
+        if (update_string == ""):
+            counter += 1
+        update_list.append(update_string)
 
+"""
+#Not used. Not all urls provide a last updated field.
+"""
+def create_history_file(url_list, update_list):
+
+    assert(len(url_list) == len(update_list))
+
+    for i in range(len(url_list)):
+        url = url_list[i]
+        timestamp = update_list[i]
+        print(url + "--" + timestamp)
+
+"""
+Pull the xml file of the Tesla website.
+@param url: the url to the xml site.
+@return soup: The extracted soup element.
+"""
 def pull_xml(url):
 
     session = requests.Session()
@@ -25,20 +65,65 @@ def pull_xml(url):
                 "lxml-xml",
                 )
 
-    #soup = soup.prettify()
-    #print(soup)
     return soup
+
+"""
+Save a local copy of the urls
+@param url_list: A list containing all of the extraced urls.
+"""
+def save_webpages(url_list):
+    session = requests.Session()
+    session.headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+
+    total = len(url_list)
+    skipped = 0
+    not_skipped = 0
+
+    for url in tqdm(url_list):
+
+        splitted_url = url.lower().split("/")
+        if ("ko_kr" in splitted_url or "es_mx" in splitted_url or "en_pr" in splitted_url or
+        "es_pr" in splitted_url or "he_il" in splitted_url or "ar_ae" in splitted_url or
+        "en_ae" in splitted_url or "en_jo" in splitted_url or "zh_hk" in splitted_url or
+        "en_hk" in splitted_url or "zh_mo" in splitted_url or "en_mo" in splitted_url or
+        "zh_tw" in splitted_url or "ja_jp" in splitted_url or "en_sg" in splitted_url or
+        "ko_kr" in splitted_url or url.__contains__("supercharger") or "cn" in splitted_url or
+        "services" in splitted_url or "stores" in splitted_url or "findus" in splitted_url or
+        "superchargers" in splitted_url or "blog" in splitted_url or url.__contains__("dc") or
+        url.__contains__("press")):
+            skipped += 1
+            continue
+
+        not_skipped += 1
+
+
+        response = session.get(url).content
+
+        soup = BeautifulSoup(response, "html.parser")
+
+        #soup = soup.prettify()
+
+        filename = url.replace("/","_").replace("www.", "").replace(".com","").replace("%","")[8:]
+        filename = "xmlPages/"+filename+".html"
+        with open(filename, "w", encoding="utf-8") as html_page:
+            html_page.write(str(soup))
+
+    print("Total: {}, Skipped: {}, Not skipped: {}".format(total, skipped, not_skipped))
 
 
 
 def main():
+    setup()
     sitemaps = ["https://www.tesla.com/sitemap.xml?page=1", "https://www.tesla.com/sitemap.xml?page=2"]
     url_list = []
+    update_list = []
     for sitemap in sitemaps:
 
         soup = pull_xml(sitemap)
-        extract_urls(soup, url_list)
-        print(len(url_list))
+        extract_urls(soup, url_list, update_list)
+
+
+    save_webpages(url_list)
 
 
 if __name__ == '__main__':
